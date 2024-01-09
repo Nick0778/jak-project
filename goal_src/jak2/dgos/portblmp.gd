2e9e5ccffcd11e6ddddebe6afb85abb22671897d
Nick0778 <149975116+Nick0778@users.noreply.github.com>
2024-01-09 19:25:02 -0300

    Added portblmp.gd in dgos folder

---
+++ goal_src/jak2/dgos/portblmp.gd
@@ -0,0 +1,18 @@
+("PORTBLMP.DGO"
+  ("target-turret-shot.o"
+   "target-turret.o"
+   ;;"port-blimp.o"
+   "jak-turret+0-ag.go"
+   "torn-turret+0-ag.go"
+   "torn-highres-ag.go"
+   "drill-turret-ext-ag.go"
+   "port-turret-ag.go"
+   "drill-turret-int-ag.go" ;;Not in original (Not sure if this is necessary)
+   "port-metal-head-boat-ag.go"
+   "tpage-2934.go"
+   "tpage-2935.go"
+   "tpage-2967.go"
+   "tpage-3071.go"
+   "tpage-3130.go"
+   "portblmp-vis.go"
+  ))
