0f031a43b66dc964939fba543eeef2d3c02ca441
Nick0778 <149975116+Nick0778@users.noreply.github.com>
2024-10-16 01:12:00 -0300

    Added custom level for turbo turret task

---
+++ custom_assets/jak3/levels/lctyturret/lctyturret.gd
@@ -0,0 +1,14 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("LCT.DGO"
+ ("cty-destroy-kg-turret.o"
+    "bombbot-h.o"
+    "bombbot-path.o"
+    "bombbot.o"
+    "cty-guard-turret-ag.go"
+    "bombbot-ag.go"
+    "lctyturret.go"
+  )
+ )
