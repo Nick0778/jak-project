17c5b439cc001407b8c9ede9bedee99c3ed64f03
Nick0778 <149975116+Nick0778@users.noreply.github.com>
2024-11-03 18:43:50 -0300

    added example custom level files

---
+++ custom_assets/jak2/levels/hover-test/hover-test.gd
@@ -0,0 +1,25 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("HRT.DGO"
+ ("hover-formation-h.o"
+  "hover-nav-control-h.o"
+  "hover-enemy-h.o"
+  "hover-nav-network.o"
+  "hover-nav-control.o"
+  "hover-enemy.o"
+  "hover-enemy-battle.o"
+  "hover-formation.o"
+  "hover-nav-edit.o"
+  "wasp-part.o"
+  "wasp.o"
+  "target-turret-shot.o"
+  "target-turret.o"
+  "drill-turret.o"
+  "hover-test-setup.o"
+  "drill-turret-ext-ag.go"
+  "jak-turret+0-ag.go"
+  "wasp-ag.go"
+  "hover-test.go"
+  ))
