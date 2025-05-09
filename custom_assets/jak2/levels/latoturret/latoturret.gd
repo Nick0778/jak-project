bb1503948e92067151538258865aca795fe7d049
Nick0778 <149975116+Nick0778@users.noreply.github.com>
2025-05-09 20:49:10 -0300

    added `latoturret` custom level files

---
+++ custom_assets/jak2/levels/latoturret/latoturret.gd
@@ -0,0 +1,13 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("LAT.DGO"
+ ("sew-gunturret.o"
+  "atoll-destroy-obs.o"
+  "atoll-destroy-turret.o"
+  "pal-gun-turret-ag.go"
+  "atoll-valve-ag.go"
+  "tpage-3383.go" ;; sewerb-minimap tpage (contains the turret HUD, but still don't work)
+  "latoturret.go"
+  ))
