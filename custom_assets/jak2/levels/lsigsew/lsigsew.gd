7eb9a31d86551ce7d0e2d6c35640619240f8b090
Nick0778 <149975116+Nick0778@users.noreply.github.com>
2025-06-23 02:29:55 -0300

    added `lsigsew` custom level files

---
+++ custom_assets/jak2/levels/lsigsew/lsigsew.gd
@@ -0,0 +1,27 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("LSS.DGO"
+ ("ai-task-h.o"
+  "bot-h.o"
+  "bot.o"
+  "bot-states.o"
+  "sig-h.o"
+  "sig-task.o"
+  "sig-shot.o"
+  "sig-plasma.o"
+  "sig.o"
+  "sig-states.o"
+  "sig-sewer-h.o" ;; type and method definitions code
+  "sig-sewer-util-funcs.o" ;; some util functions code
+  "sig6-course.o" ;; bot course code
+  "grunt.o"
+  "juicer.o"
+  "sewer-obs2.o"
+  "sig-ag.go"
+  "grunt-ag.go"
+  "juicer-ag.go"
+  "sew-mine-a-ag.go"
+  "lsigsew.go"
+  ))
